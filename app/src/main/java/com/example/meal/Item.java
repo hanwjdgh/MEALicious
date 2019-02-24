43e460cfd2c130adf50c39c88d32072507305ab4
hanwjdgh <hanwjdgh@naver.com>
2019-02-24 16:36:10 +0900

    update Search

--- app/src/main/java/com/example/meal/Item.java
+++ app/src/main/java/com/example/meal/Item.java
@@ -1,7 +1,7 @@
 package com.example.meal;
 
 public class Item {
-    int image;
+    int image,image1;
     String title;
 
     public int getImage() {
@@ -11,8 +11,9 @@ public class Item {
         return this.title;
     }
 
-    public Item(int image, String title) {
+    public Item(int image, int imag1, String title) {
         this.image = image;
+        this.image1 = imag1;
         this.title = title;
     }
 }
