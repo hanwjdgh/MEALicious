cc6a77e714ee135af6618c5db513a7616a2abba8
Sang-won <style8914@naver.com>
2019-02-25 16:06:14 +0900

    update order fragment

--- app/src/main/java/com/example/meal/Fragment/OrderFragment.java
+++ app/src/main/java/com/example/meal/Fragment/OrderFragment.java
@@ -1,16 +1,38 @@
 package com.example.meal.Fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.meal.R;
 
 public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        Button agree = view.findViewById(R.id.agree);
+        Button lookup = view.findViewById(R.id.lookup);
+
+        agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.personal_information);
+                dialog.show();
+            }
+        });
+
+        lookup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// 지갑 화면 켜지기
+
+            }
+        });
+        return view;
     }
 }
