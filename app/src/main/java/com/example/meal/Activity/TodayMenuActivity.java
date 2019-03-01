281679e94558aa52833c7d101fcf9357e6b0fd92
hanwjdgh <hanwjdgh@naver.com>
2019-03-01 11:17:06 +0900

    MenuActivity

--- app/src/main/java/com/example/meal/Activity/TodayMenuActivity.java
+++ app/src/main/java/com/example/meal/Activity/TodayMenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.meal.Activity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +15,7 @@ import android.widget.Toast;
 
 import com.example.meal.R;
 
-public class TodayMenuActivity extends ServiceActivity {
+public class TodayMenuActivity extends AppCompatActivity {
     View view, view1;
     LinearLayout linearLayout1, linearLayout2;
     RelativeLayout relativeLayout;
@@ -23,6 +25,10 @@ public class TodayMenuActivity extends ServiceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todaymenu);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_name);
 
         Intent intent = getIntent();
         check = Integer.parseInt(intent.getStringExtra("round"));
@@ -53,6 +59,7 @@ public class TodayMenuActivity extends ServiceActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(TodayMenuActivity.this, MenuDetailActivity.class);
                 startActivity(intent);
+                overridePendingTransition(0,0);
             }
         });
     }
@@ -66,10 +73,57 @@ public class TodayMenuActivity extends ServiceActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        menu.getItem(0).getSubMenu().getItem(ServiceActivity.check_lan).setChecked(true);
+        menu.getItem(0).setTitle(ServiceActivity.lan[ServiceActivity.check_lan]);
+        for(int i=0; i<4; i++)
+            menu.getItem(0).getSubMenu().getItem(i).setTitle(ServiceActivity.menu_title[ServiceActivity.check_lan][i]);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        item.setChecked(true);
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+                return true;
+            case R.id.lag:
+                return true;
+            case R.id.kor:
+                ServiceActivity.check_lan = 0;
+                Toast.makeText(this, "한국어", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.eng:
+                ServiceActivity.check_lan = 1;
+                Toast.makeText(this, "영어", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.cin:
+                ServiceActivity.check_lan = 2;
+                Toast.makeText(this, "중국어", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.jap:
+                ServiceActivity.check_lan = 3;
+                Toast.makeText(this, "일어", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.map:
+                Dialog dialog = new Dialog(TodayMenuActivity.this);
+                dialog.setContentView(R.layout.location_map);
+                dialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        invalidateOptionsMenu();
     }
 }
