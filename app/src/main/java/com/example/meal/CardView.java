6adef1d399a064792a3d625d1d0aa65f28bb3918
hanwjdgh <hanwjdgh@naver.com>
2019-03-06 16:22:42 +0900

    Wallet

--- app/src/main/java/com/example/meal/CardView.java
+++ app/src/main/java/com/example/meal/CardView.java
@@ -2,12 +2,20 @@ package com.example.meal;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class CardView extends android.support.v7.widget.AppCompatImageView {
+public class CardView extends android.support.v7.widget.CardView {
+    Context context;
+    LinearLayout linearLayout;
+    int cardImage;
 
     public CardView(Context context, int cardIdDrawable) {
         super(context);
-        setImageDrawable(context.getResources().getDrawable(cardIdDrawable));
+        this.context = context;
+        this.cardImage = cardIdDrawable;
+        init();
+        //setImageDrawable(context.getResources().getDrawable(cardIdDrawable));
     }
 
     public CardView(Context context) {
@@ -20,5 +28,11 @@ public class CardView extends android.support.v7.widget.AppCompatImageView {
 
     public CardView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    public void init(){
+        View v = inflate(getContext(),R.layout.item_flight,this);
+        linearLayout = v.findViewById(R.id.layout);
+        linearLayout.setBackgroundColor(cardImage);
     }
 }
