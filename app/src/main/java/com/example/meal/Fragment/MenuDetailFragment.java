33c1d1332ade9d611824479f9a407a9d24a8fdd9
Sang-won <style8914@naver.com>
2019-03-18 06:40:31 +0900

    Merge branches 'Sang-won' and 'master' of https://github.com/hanwjdgh/MEALicious into Sang-won
    # Conflicts:
    #	app/src/main/java/com/example/meal/Fragment/MenuDetailFragment.java

--- app/src/main/java/com/example/meal/Fragment/MenuDetailFragment.java
+++ app/src/main/java/com/example/meal/Fragment/MenuDetailFragment.java
@@ -37,7 +37,7 @@ public class MenuDetailFragment extends Fragment {
     int menuname[] = {R.drawable.menu_1, R.drawable.menu_2, R.drawable.menu_3};
     int Images[] = {R.drawable.meal_1, R.drawable.meal_2, R.drawable.meal_3};
     int videos[] = {R.raw.roast, R.raw.slice, R.raw.roast};
-    String meals[] = {"불고기 덮밥", "낙지 덮밥", "생선 크림 파스타"};
+    String meals[] = {"낙지 덮밥", "생선 크림 파스타", "흰 쌀죽"};
     DetailAdapter detailAdapter;
     OrderAdapter orderAdapter;
     ImageView confirm, cancel, check_order_image;
