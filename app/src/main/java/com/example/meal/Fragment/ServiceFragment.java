b88df8e72cf037c9a4e7eada55f706c906691346
Sang-won <style8914@naver.com>
2019-03-17 16:14:55 +0900

    update service

--- app/src/main/java/com/example/meal/Fragment/ServiceFragment.java
+++ app/src/main/java/com/example/meal/Fragment/ServiceFragment.java
@@ -1,20 +1,33 @@
 package com.example.meal.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
+import com.example.meal.Activity.MainActivity;
 import com.example.meal.Activity.ServiceActivity;
 import com.example.meal.R;
 
 public class ServiceFragment extends Fragment {
+    private int animationCounter = 1;
+    private Handler imageSwitcherHandler;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_service, container, false);
-        Button button = view.findViewById(R.id.next);
+        final ImageSwitcher imageSwitcher = view.findViewById(R.id.service);
+        ImageView button = view.findViewById(R.id.next);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +35,48 @@ public class ServiceFragment extends Fragment {
                 ServiceActivity.mViewPager.setCurrentItem(2);
             }
         });
+
+        Animation in  = AnimationUtils.loadAnimation(getContext(), R.anim.slide_up);
+        Animation out = AnimationUtils.loadAnimation(getContext(), R.anim.slide_up);
+
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                return imageView;
+            }
+        });
+
+        imageSwitcher.setInAnimation(in);
+        imageSwitcher.setOutAnimation(out);
+
+        imageSwitcherHandler = new Handler(Looper.getMainLooper());
+        imageSwitcherHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                switch (animationCounter++) {
+                    case 1:
+                        imageSwitcher.setImageResource(R.drawable.service_1);
+                        break;
+                    case 2:
+                        imageSwitcher.setImageResource(R.drawable.service_2);
+                        break;
+                    case 3:
+                        imageSwitcher.setImageResource(R.drawable.service_3);
+                        break;
+                    case 4:
+                        imageSwitcher.setImageResource(R.drawable.service_4);
+                        break;
+                }
+                animationCounter %= 5;
+                if(animationCounter == 0 ) animationCounter = 1;
+
+                imageSwitcherHandler.postDelayed(this, 4500);
+            }
+        });
+
+
         return view;
     }
 }
