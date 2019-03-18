59376db7117e00c7aac72d969cf7fe1446c7c88b
Sang-won <style8914@naver.com>
2019-03-19 05:54:44 +0900

    update ui

--- app/src/main/java/com/example/meal/Fragment/AboutFragment.java
+++ app/src/main/java/com/example/meal/Fragment/AboutFragment.java
@@ -5,12 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.meal.R;
 
 public class AboutFragment extends Fragment {
+    ImageView about;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_d_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_d_about, container, false);
+
+        about = view.findViewById(R.id.about);
+
+
+        return view;
     }
 }
